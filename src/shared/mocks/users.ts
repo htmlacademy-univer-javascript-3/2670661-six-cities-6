2400c4e6d7c46f08809c5196d8e955b3d0c00296
pasha <pashabezk@mail.ru>
2025-10-16 01:26:18 +0300

    feat: add review components

---
+++ src/shared/mocks/users.ts
@@ -0,0 +1,14 @@
+import {User} from '../../entities/review/model/types.ts';
+
+export const usersMock: User[] = [
+  {
+    id: 1,
+    name: 'Max',
+    photoUrl: 'img/avatar-max.jpg',
+  },
+  {
+    id: 2,
+    name: 'Angelina',
+    photoUrl: 'img/avatar-angelina.jpg',
+  },
+];
