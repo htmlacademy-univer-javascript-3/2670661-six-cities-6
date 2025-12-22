175d363c6dfa07c7bee61d23cd3ab18d6ee65b0f
pasha <pashabezk@mail.ru>
2025-12-23 01:03:35 +0300

    feat: implement offers sort

--- src/features/offers-manager/model/offers-slice.ts
+++ src/features/offers-manager/model/offers-slice.ts
@@ -2,7 +2,8 @@ import {createSlice, PayloadAction} from '@reduxjs/toolkit';
 import {DEFAULT_CITY} from '../../../entities/city/model/constants.ts';
 import {extractCities} from '../../../entities/city/model/data-mappers.ts';
 import {CitiesMap, City} from '../../../entities/city/model/types.ts';
-import {groupOffersByCity, mapDtoToOffer} from '../../../entities/offer/model/data-mappers.ts';
+import {OfferSortOption} from '../../../entities/offer/model/constants.ts';
+import {applySortToOffers, groupOffersByCity, mapDtoToOffer} from '../../../entities/offer/model/data-mappers.ts';
 import {Offer, OfferDto, OffersByCity} from '../../../entities/offer/model/types.ts';
 import {ReducerName} from '../../../shared/enums/reducer-names.ts';
 import {createAppThunk} from '../../../shared/redux-helpers/typed-thunk.ts';
@@ -15,6 +16,7 @@ type OffersState = {
   favoriteOffersCount: number;
   currentCityOffers: Offer[];
   activeOfferId: Offer['id'] | null;
+  sortOption: OfferSortOption;
   isOffersLoading: boolean;
 };
 
@@ -25,6 +27,7 @@ const initialState: OffersState = {
   favoriteOffersCount: 0,
   currentCityOffers: [],
   activeOfferId: null,
+  sortOption: OfferSortOption.popular,
   isOffersLoading: false,
 };
 
@@ -45,14 +48,25 @@ export const offersSlice = createSlice({
       const city = action.payload;
       state.currentCity = city;
       state.currentCityOffers = state.offers[city.name];
+      state.sortOption = OfferSortOption.popular;
     },
     setActiveOffer: (state: OffersState, action: PayloadAction<Offer['id']>) => {
       state.activeOfferId = action.payload;
+    },
+    setOffersSort: (state: OffersState, action: PayloadAction<OfferSortOption>) => {
+      const sort = action.payload;
+      state.sortOption = sort;
+      if (sort === OfferSortOption.popular) {
+        state.currentCityOffers = state.offers[state.currentCity.name];
+      } else {
+        state.currentCityOffers = applySortToOffers(state.currentCityOffers, sort);
+      }
     },
   },
   extraReducers: (builder) => {
     builder.addCase(loadOffers.pending, (state) => {
       state.isOffersLoading = true;
+      state.sortOption = OfferSortOption.popular;
     }).addCase(loadOffers.fulfilled, (state, action) => {
       const offers = groupOffersByCity(action.payload.map(mapDtoToOffer));
       state.isOffersLoading = false;
@@ -67,4 +81,4 @@ export const offersSlice = createSlice({
 });
 
 export const offersReducer = offersSlice.reducer;
-export const {setCity, setActiveOffer} = offersSlice.actions;
+export const {setCity, setActiveOffer, setOffersSort} = offersSlice.actions;
