2742a4557dcfc461a22da36ac56f45ef890ff4e9
pasha <pashabezk@mail.ru>
2025-12-23 03:39:45 +0300

    feat: add favorites empty

--- src/pages/favorites-page/favorites-page.tsx
+++ src/pages/favorites-page/favorites-page.tsx
@@ -1,9 +1,12 @@
 import {FC} from 'react';
+import {NavLink} from 'react-router-dom';
 import {OffersByCity} from '../../entities/offer/model/types.ts';
 import {HeaderLogoLink} from '../../shared/components/header-logo-link.tsx';
+import {RoutePath} from '../../shared/enums/routes.ts';
 import {useAppSelector} from '../../shared/redux-helpers/typed-hooks.ts';
 import {HeaderUserInfo} from '../../widgets/common-components/header-user-info.tsx';
 import {FavoriteOfferCardList} from './cards/favorite-offer-card-list.tsx';
+import {FavoritesEmpty} from './favorites-empty.tsx';
 
 export const FavoritesPage: FC = () => {
   const offers = useAppSelector((state) => state.offers.offers);
@@ -32,17 +35,18 @@ export const FavoritesPage: FC = () => {
 
       <main className="page__main page__main--favorites">
         <div className="page__favorites-container container">
-          <section className="favorites">
-            <h1 className="favorites__title">Saved listing</h1>
-            {favoritesCounter === 0 && <p>You have not bookmarked any offers yet</p>}
-            <FavoriteOfferCardList offers={favorites}/>
-          </section>
+          {favoritesCounter === 0 ? <FavoritesEmpty/> : (
+            <section className="favorites">
+              <h1 className="favorites__title">Saved listing</h1>
+              <FavoriteOfferCardList offers={favorites}/>
+            </section>
+          )}
         </div>
       </main>
       <footer className="footer container">
-        <a className="footer__logo-link" href="main.html">
+        <NavLink className="footer__logo-link" to={'/' + RoutePath.MainPage}>
           <img className="footer__logo" src="img/logo.svg" alt="6 cities logo" width="64" height="33"/>
-        </a>
+        </NavLink>
       </footer>
     </div>
   );
