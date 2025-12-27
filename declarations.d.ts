c32bbd5f5ee012eca065e105cce18b5c0ed939bb
pasha <pashabezk@mail.ru>
2025-12-27 03:36:45 +0300

    feat: support inert attribute

---
+++ declarations.d.ts
@@ -0,0 +1,13 @@
+import {AriaAttributes, DOMAttributes} from 'react';
+
+declare module 'react' {
+  interface HTMLAttributes<T> extends AriaAttributes, DOMAttributes<T> {
+    /**
+     * Indicates that the browser will ignore this element and its descendants,
+     * preventing some interactions and hiding it from assistive technology.
+     * @see https://developer.mozilla.org/en-US/docs/Web/HTML/Global_attributes/inert
+     * @todo Remove this stub declaration after https://github.com/facebook/react/pull/24730 is merged.
+     */
+    inert?: '';
+  }
+}
